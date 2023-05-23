e35fa21d72b2d025e4bca971c87f34b4ad9af424
yungs17 <yungs17@users.noreply.github.com>
2023-05-24 01:00:10 +0900

    s

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
+(async () => {
+  // Start your app
+  await app.start(8080);
+
+  console.log("Jonathan On");
+})();
+
 app.command("/test", async ({ ack }) => {
   await ack();
   console.log("req received");
