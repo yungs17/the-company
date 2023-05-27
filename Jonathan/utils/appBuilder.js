10ffea22daffd9594619c477161292e1cdf0c72d
yungs17 <yungs17@users.noreply.github.com>
2023-05-28 03:03:14 +0900

    misc

--- Jonathan/utils/appBuilder.js
+++ Jonathan/utils/appBuilder.js
@@ -1,20 +1,26 @@
 import fs from "fs/promises";
 import path from "path";
 
-const registerCommands = async (app) => {
-  const commandsDir = path.resolve("./Jonathan/commands");
-  const filenames = await fs.readdir(commandsDir);
+class AppBuilder {
+  constructor(app) {
+    this.app = app;
+    this.commandsDir = path.resolve("./Jonathan/commands");
+  }
 
-  await Promise.all(
-    filenames.map(async (filename) => {
-      console.log(filename);
-      if (filename === "index.js") return;
-      const moduleName = path.basename(filename, ".js");
-      const modulePath = `${commandsDir}/${filename}`;
-      const commandModule = await import(modulePath);
-      app.command(`/${moduleName}`, commandModule.default);
-    })
-  );
-};
+  async registerCommands() {
+    const filenames = await fs.readdir(this.commandsDir);
 
-export { registerCommands };
+    await Promise.all(
+      filenames.map(async (filename) => {
+        if (filename === "index.js") return;
+        const moduleName = path.basename(filename, ".js");
+        console.log("Registering slack command: " + moduleName);
+        const modulePath = `${this.commandsDir}/${filename}`;
+        const commandModule = await import(modulePath);
+        this.app.command(`/${moduleName}`, commandModule.default);
+      })
+    );
+  }
+}
+
+export default AppBuilder;
--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 import pkg from "@slack/bolt";
 import { config } from "dotenv";
-import * as appBuilder from "./Jonathan/utils/appBuilder.js";
+import AppBuilder from "./Jonathan/utils/AppBuilder.js";
+import DBHandler from "./Jonathan/utils/DBHandler.js";
+import * as toadSchedule from "toad-scheduler";
 const { App } = pkg;
 
 config();
+
+const dbConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "main",
+  connectionLimit: 1,
+};
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -13,7 +23,22 @@ const app = new App({
 (async () => {
   await app.start(8080);
 
-  await appBuilder.registerCommands(app);
+  const appBuiler = new AppBuilder(app);
+  const dbHandler = new DBHandler(dbConfig);
+
+  appBuiler.registerCommands();
+
+  // jonathan logics under here
+  const processTasks = async () => {
+    // go through tasks from db and call work function according to their types
+    const tasks = await dbHandler.getAllTasks();
+    console.log(tasks);
+  };
+
+  const toadScheduler = new toadSchedule.ToadScheduler();
+  const toadTask = new toadSchedule.Task("Jonathan working", processTasks);
+  const toadJob = new toadSchedule.SimpleIntervalJob({ seconds: 5 }, toadTask);
+  toadScheduler.addSimpleIntervalJob(toadJob);
 
   console.log("Jonathan On");
 })();
